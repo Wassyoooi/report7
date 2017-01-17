f8138d93a23edfd2f21d4a5122b9cda48df126b8
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2017-01-17 23:06:38 +0900

    Ver2

--- src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
@@ -3,26 +3,27 @@ package jp.ac.uryukyu.ie.e165726;
 import java.io.*;
 
 public class PartSelection {
-    private int part;
+    String part;
 
-    public static void PartSelection()throws IOException {
-        System.out.println("パートを選ぶドン!");
-        System.out.println("part1,part2,part3");
-        System.out.println();
-        InputStreamReader is = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(is);
-        System.out.println("part1を選ぶなら「1」、part2を選ぶなら「2」、part3を選ぶなら「3」を入力してください。");
-        String part = br.readLine();
-        System.out.println("さあ、はじまるドン!!");
+    public PartSelection(String part)throws IOException {
+        this.part = part;
+    }
 
+    public int select()throws IOException{   //PartClassから正解数を受け取る
+        int ansnum = 0;
+        Part1Class part1 = new Part1Class();
+        Part2Class part2 = new Part2Class();
+        Part3Class part3 = new Part3Class();
         if(part == "1"){
             ansnum = Part1Class."メソッド"
         }else if(part == "2"){
-
+            ansnum = Part2Class."メソッド"
         }else if(part == "3"){
-
+            ansnum = Part3Class."メソッド"
         }else{
             System.out.println("入力し直してください");
         }
+        System.out.println("正解数は10問中%d問です。",ansnum);
+        return ansnum;
     }
 }
