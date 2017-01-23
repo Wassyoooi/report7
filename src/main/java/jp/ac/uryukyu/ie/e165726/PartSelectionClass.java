a073286aa3f14adc9211cfa22026a002474ab0fe
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2017-01-23 16:09:15 +0900

    Ver11 Most of Main.java & PartSelectionClass has been wrote.

--- src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
@@ -15,7 +15,7 @@ public class PartSelectionClass {
             System.out.println("part1を選ぶなら「1」、part2を選ぶなら「2」、part3を選ぶなら「3」を入力してください。おわる時はそれ以外のキーを入力してください。");
             String part = br.readLine();
             int p = Integer.parseInt(part);
-            if(p<=3) System.out.println("さあ、はじまるドン!!");
+            if(p<0 && p<=3) System.out.println("さあ、はじまるドン!!");
             return p;
         }catch (NumberFormatException e){ //数値以外のキーが入力された場合
             int p = 256;
@@ -24,18 +24,18 @@ public class PartSelectionClass {
     }
 
     public int callPart(int part) throws IOException {   //選択したパートを呼び出す
-        int ansnum = 0;
-        if (part == 1) {
+        int ansnum = 0; //正解数
+        if (part == 1) {            //パート1を選択
             Part1 part1 = new Part1();
             ansnum = part1.question1(ansnum);
             System.out.println("正解数は5問中" + ansnum + "問です。");
-        } else if (part == 2) {
+        } else if (part == 2) {     //パート2を選択
             //ansnum = Part2Class. "メソッド"
             //System.out.println("正解数は5問中%d問です。", ansnum);
-        } else if (part == 3) {
+        } else if (part == 3) {     //パート3を選択
             //ansnum = Part3Class. "メソッド"
             //System.out.println("正解数は5問中%d問です。", ansnum);
-        } else {
+        } else {                    //問題をやめる
             System.out.println("また遊んでネ!");
             ansnum = 256;
         }
@@ -43,20 +43,20 @@ public class PartSelectionClass {
     }
 
     public int judge(int ansnum,boolean AlreadyOutput) { //全問正解かどうか判断
-        int AllCorrect;
-        if (ansnum < 5) {
+        int CorrectJudge; //全問正解ではないのなら1,全問正解なら2,問題をやめる場合3が代入される。
+        if (ansnum < 5) {       //全問正解ではない時
             System.out.println("全問正解じゃないドン!");
             System.out.println("もう一回遊べるドン!");
-            AllCorrect = 1;
-        }else if(ansnum == 5){
+            CorrectJudge = 1;
+        }else if(ansnum == 5){  //全問正解の時
             if(AlreadyOutput == false){
                 System.out.println("全問正解!!おめでとう!!");
             }
-            AllCorrect = 2;
-        }else{
-            AllCorrect = 3;
+            CorrectJudge = 2;
+        }else{                  //問題をやめる時
+            CorrectJudge = 3;
         }
-        return AllCorrect;
+        return CorrectJudge;
     }
 
 }
