448109b96c701d35257607ccd3e3c44a80b437a1
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2017-01-23 15:38:50 +0900

    Ver10

--- src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/PartSelectionClass.java
@@ -3,19 +3,24 @@ package jp.ac.uryukyu.ie.e165726;
 import java.io.*;
 
 public class PartSelectionClass {
-    public int select() throws IOException {   //ユーザーにpartを入力させる
-        //Part2Class part2 = new Part2Class();
-        //Part3Class part3 = new Part3Class();
-        System.out.println("パートを選ぶドン!");
-        System.out.println("part1,part2,part3,おわる");
-        System.out.println();
-        InputStreamReader is = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(is);
-        System.out.println("part1を選ぶなら「1」、part2を選ぶなら「2」、part3を選ぶなら「3」を入力してください。おわる時はそれ以外のキーを入力してください。");
-        String part = br.readLine();
-        int p = Integer.parseInt(part);
-        if(p<=3) System.out.println("さあ、はじまるドン!!");
-        return p;
+    public int select() throws IOException {//ユーザーにpartを入力させる
+        try{
+            //Part2Class part2 = new Part2Class();
+            //Part3Class part3 = new Part3Class();
+            System.out.println("パートを選ぶドン!");
+            System.out.println("part1,part2,part3,おわる");
+            System.out.println();
+            InputStreamReader is = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(is);
+            System.out.println("part1を選ぶなら「1」、part2を選ぶなら「2」、part3を選ぶなら「3」を入力してください。おわる時はそれ以外のキーを入力してください。");
+            String part = br.readLine();
+            int p = Integer.parseInt(part);
+            if(p<=3) System.out.println("さあ、はじまるドン!!");
+            return p;
+        }catch (NumberFormatException e){ //数値以外のキーが入力された場合
+            int p = 256;
+            return p;
+        }
     }
 
     public int callPart(int part) throws IOException {   //選択したパートを呼び出す
