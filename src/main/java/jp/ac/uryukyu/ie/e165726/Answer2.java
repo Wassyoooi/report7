3098d977f32ca350ce0cea2de348f8735cb43961
e165744 <e165744@ie.u-ryukyu.ac.jp>
2017-01-24 10:02:02 +0900

    Ver20 comments have been written

--- src/main/java/jp/ac/uryukyu/ie/e165726/Answer2.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Answer2.java
@@ -4,44 +4,44 @@ package jp.ac.uryukyu.ie.e165726;
  * Created by e165744 on 2017/01/23.
  */
 public class Answer2 {
-    public int answer2(int i, String ans, int ansnum){
+    public int answer2(int i, String ans, int ansNum){
         int q = Integer.parseInt(ans);
-        if (i == 0){
-            if(q == 3) {
+        if (i == 0){  //問題1
+            if(q == 3) {  //選択した番号が3の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。");
             }
-        }else if(i == 1){
-            if(q == 1){
+        }else if(i == 1){  //問題2
+            if(q == 1){  //選択した番号が1の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は1です。");
             }
-        }else if(i == 2){
-            if(q == 2){
+        }else if(i == 2){  //問題3
+            if(q == 2){  //選択した番号が2の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は2です。");
             }
-        }else if(i == 3){
-            if(q == 3){
+        }else if(i == 3){  //問題4
+            if(q == 3){  //選択した番号が3の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。");
             }
-        }else{
-            if(q == 3){
+        }else{  //問題5
+            if(q == 3){  //選択した番号が3の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。");
             }
         }
-        return ansnum;
+        return ansNum;
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e165726/Answer3.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Answer3.java
@@ -4,44 +4,44 @@ package jp.ac.uryukyu.ie.e165726;
  * Created by e165744 on 2017/01/23.
  */
 public class Answer3 {
-    public int answer3(int i, String ans, int ansnum){
+    public int answer3(int i, String ans, int ansNum){
         int q = Integer.parseInt(ans);
-        if (i == 0){
-            if(q == 2) {
+        if (i == 0){  //問題1
+            if(q == 2) {  //選択した番号が2の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は2です。");
             }
-        }else if(i == 1){
-            if(q == 4){
+        }else if(i == 1){  //問題2
+            if(q == 4){  //選択した番号が2の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は4です。");
             }
-        }else if(i == 2){
-            if(q == 3){
+        }else if(i == 2){  //問題3
+            if(q == 3){  //選択した番号が3の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。");
             }
-        }else if(i == 3){
-            if(q == 2){
+        }else if(i == 3){  //問題4
+            if(q == 2){  //選択した番号が2の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は2です。");
             }
-        }else{
-            if(q == 1){
+        }else{  //問題5
+            if(q == 1){  //選択した番号が1の場合
                 System.out.println("正解！！");
-                ansnum += 1;
+                ansNum += 1;
             }else{
                 System.out.println("残念。正解は1です。");
             }
         }
-        return ansnum;
+        return ansNum;
     }
 }
