6a49ccecc1dec10057681a69da2e355bc61851b3
e165744 <e165744@ie.u-ryukyu.ac.jp>
2017-01-28 22:47:27 +0900

    Ver25 comments have been removed

--- src/main/java/jp/ac/uryukyu/ie/e165726/Answer2.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Answer2.java
@@ -4,38 +4,38 @@ package jp.ac.uryukyu.ie.e165726;
  * Created by e165744 on 2017/01/23.
  */
 public class Answer2 {
-    public int answer2(int i, String ans, int ansNum){
+    public int answer2(int i, String ans, int ansNum){// 選んだ答えが当たっているか答え合わせする。
         int q = Integer.parseInt(ans);
         if (i == 0){  //問題1
-            if(q == 3) {  //選択した番号が3の場合
+            if(q == 3) {
                 System.out.println("正解！！");
                 ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。"); System.out.println();
             }
         }else if(i == 1){  //問題2
-            if(q == 1){  //選択した番号が1の場合
+            if(q == 1){
                 System.out.println("正解！！");
                 ansNum += 1;
             }else{
                 System.out.println("残念。正解は1です。"); System.out.println();
             }
         }else if(i == 2){  //問題3
-            if(q == 2){  //選択した番号が2の場合
+            if(q == 2){
                 System.out.println("正解！！");
                 ansNum += 1;
             }else{
                 System.out.println("残念。正解は2です。"); System.out.println();
             }
         }else if(i == 3){  //問題4
-            if(q == 3){  //選択した番号が3の場合
+            if(q == 3){
                 System.out.println("正解！！");
                 ansNum += 1;
             }else{
                 System.out.println("残念。正解は3です。"); System.out.println();
             }
         }else{  //問題5
-            if(q == 3){  //選択した番号が3の場合
+            if(q == 3){
                 System.out.println("正解！！");
                 ansNum += 1;
             }else{
--- src/main/java/jp/ac/uryukyu/ie/e165726/Part3.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Part3.java
@@ -5,16 +5,16 @@ public class Part3 {
     public int question3(int ansNum)throws IOException
     {
         Answer3 as3 = new Answer3();
-        String[] word = {"drawback","encryption","abstraction","decision","identififer"};  //String型で配列wordに5個の単語を用意
+        String[] word = {"drawback","encryption","abstraction","decision","identifier"};
 
-        String[] selection;  //String型で選択肢の配列を用意
-        selection = new String[5];  //選択肢のインスタンスを5個用意
+        String[] selection;
+        selection = new String[5];
 
-        selection[0] = "1.四捨五入する 2.欠点 3.抽象 4.決定";       //答え2
-        selection[1] = "1.繰り下げる 2.欠点 3.抽象 4.暗号化";        //答え4
-        selection[2] = "1.映像 2.繰り下げる 3.抽象 4.暗号化";  //答え3
-        selection[3] = "1.暗号化 2.決定 3.映像 4.ID";     //答え2
-        selection[4] = "1.ID 2.欠点 3.責任 4.能力がない";     //答え1
+        selection[0] = "1.四捨五入する 2.欠点 3.抽象 4.決定"; //2
+        selection[1] = "1.繰り下げる 2.欠点 3.抽象 4.暗号化"; //4
+        selection[2] = "1.映像 2.繰り下げる 3.抽象 4.暗号化"; //3
+        selection[3] = "1.暗号化 2.決定 3.映像 4.ID"; //2
+        selection[4] = "1.ID 2.欠点 3.責任 4.能力がない"; //1
 
         for (int i = 0; i < 5; i++)
         {
@@ -22,7 +22,7 @@ public class Part3 {
             System.out.println(selection[i]);
             InputStreamReader is = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(is);
-            String ans = br.readLine();  //入力した文字列をString型のansに代入する
+            String ans = br.readLine();
             System.out.println("あなたの答えは" + ans + "ですね。");
             ansNum = as3.answer3(i, ans, ansNum);
         }
