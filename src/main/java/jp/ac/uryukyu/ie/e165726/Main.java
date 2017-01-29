939442b1ec3d43fc2742293451e2d942ec1071d0
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2017-01-29 17:51:57 +0900

    Ver30 Add to comment.

--- src/main/java/jp/ac/uryukyu/ie/e165726/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Main.java
@@ -6,12 +6,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args)throws IOException{
-        PartSelection partSelection = new PartSelection();
+        PartSelection partSelection = new PartSelection(); //PartSelectionのインスタンス生成
         int partNum;     //選択パート番号
         int ansNum;      //正解数
         boolean isAlreadyOutput = true; //全問正解メッセージを出力したか判定
 
-        System.out.println("苦しんで覚える英単語、単語の達人");
+        System.out.println("単語の達人");
         System.out.println();
         System.out.println("このプログラムはIT系によく出てくる英単語を覚えるために作られました。各パート5単語ずつ出題されるので、4択の中から正解を選んでください。すべて正解するまで抜けられません。");
         System.out.println();
