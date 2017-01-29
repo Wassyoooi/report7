ae30fc72d86807b92a61781bd22a533bd0677fa4
e165719 <e165719@ie.u-ryukyu.ac.jp>
2017-01-29 17:34:10 +0900

    Update Main.java

--- src/main/java/jp/ac/uryukyu/ie/e165726/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165726/Main.java
@@ -10,7 +10,7 @@ public class Main {  //165726H 兼島光平担当
         int ansNum;      //正解数
         boolean isAlreadyOutput = true; //全問正解メッセージを出力したか判定
 
-        System.out.println("苦しんで覚える英単語、単語の達人");
+        System.out.println("単語の達人");
         System.out.println();
         System.out.println("このプログラムはIT系によく出てくる英単語を覚えるために作られました。各パート5単語ずつ出題されるので、4択の中から正解を選んでください。すべて正解するまで抜けられません。");
         System.out.println();
